676439f3723f2bc7e301ef8dece031d83e241b38
nananeko <nananeko@pop-os.localdomain>
2023-08-01 20:10:31 +0200

    Connected Tweet_Service to Elastic container

--- Twitter-Backend/tweet_service/configs/elastic.go
+++ Twitter-Backend/tweet_service/configs/elastic.go
@@ -2,13 +2,10 @@ package configs
 
 import (
 	"github.com/elastic/go-elasticsearch/v7"
-	"log"
 	"tweet_service/startup/config"
 )
 
 func ConnectToElastic(config *config.Config) (*elasticsearch.Client, error) {
-
-	log.Println(config.ELASTICSEARCH_HOSTS)
 
 	cfg := elasticsearch.Config{
 		Addresses: []string{
@@ -19,12 +16,6 @@ func ConnectToElastic(config *config.Config) (*elasticsearch.Client, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	info, err := client.Info()
-	if err != nil {
-		return nil, err
-	}
-	log.Println(info)
 
 	return client, nil
 }
